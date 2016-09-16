4eb1bbebc3f61df2dc4e21f997ce0b0b99c5d363
ali yeysides <ali.yeysides@gmail.com>
2016-09-16 18:36:02 -0400

    npm init

---
+++ js/app.jsx
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import ReactDom from 'react-dom';
+
+class Home extends React.Component {
+  render() {
+    return <h1>Hello</h1>
+  }
+}
+
+ReactDOM.render(<Hello/>, document.getElementById('hello'));
