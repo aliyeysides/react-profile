73a062d62aaa240315d03602afed4ccb3ea8a18b
ali yeysides <ali.yeysides@gmail.com>
2016-09-17 17:20:02 -0400

    Import assets from react-bootstrap and setup example navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import { Navbars } from 'react-bootstrap';
+import ReactDOM from 'react-dom';
+import { Navbar } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import { NavItem } from 'react-bootstrap';
+import { NavDropdown } from 'react-bootstrap';
+import { MenuItem } from 'react-bootstrap';
 
-class Navbar extends React.Component {
+class MyNavbar extends React.Component {
     render() {
-        return <div>Navbar</div>
+        return (
+            <Navbar>
+                <Navbar.Header>
+                <Navbar.Brand>
+                    <a href="#">React-Bootstrap</a>
+                </Navbar.Brand>
+                </Navbar.Header>
+                <Nav>
+                <NavItem eventKey={1} href="#">Link</NavItem>
+                <NavItem eventKey={2} href="#">Link</NavItem>
+                <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                    <MenuItem eventKey={3.1}>Action</MenuItem>
+                    <MenuItem eventKey={3.2}>Another action</MenuItem>
+                    <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                    <MenuItem divider />
+                    <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                </NavDropdown>
+                </Nav>
+            </Navbar>
+        )
+
     }
 }
-
-export default Navbar;
+export default MyNavbar;
