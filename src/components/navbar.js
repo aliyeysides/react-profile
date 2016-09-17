2036ee989007c3205e88afa62ac7d17f55215c6e
ali yeysides <ali.yeysides@gmail.com>
2016-09-17 15:30:22 -0400

    Add navbar component

---
+++ src/components/navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Navbars } from 'react-bootstrap';
+
+class Navbar extends React.Component {
+    render() {
+        return <div>Navbar</div>
+    }
+}
+
+export default Navbar;
