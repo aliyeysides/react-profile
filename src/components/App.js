3677d6d29cdb407c10289cf3e457f4e41d373f85
ali yeysides <ali.yeysides@gmail.com>
2016-09-18 18:45:06 -0400

    Nest footer component within main App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navigation from './navigation.js';
+import Footer from './footer.js';
 
 export default class App extends React.Component {
     render() {
         return (
             <div className="exterior">
-                <Navigation/>
+                <Navigation />
+                <Footer />
             </div>
         )
     }
