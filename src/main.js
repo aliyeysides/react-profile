d1caecc562b07d78e82ff9fed0096785346cf247
ali yeysides <ali.yeysides@gmail.com>
2016-09-17 21:30:50 -0400

    Modify require dir for stylesheets

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-require('../css/app.css');
+require('../scss/app.scss');
 
 ReactDOM.render(<App/>, document.getElementById('app'));
