4e8713087ed55be308ff1dbae6f81a169a8a9894
ali yeysides <ali.yeysides@gmail.com>
2016-09-23 12:51:22 -0400

    Add ProjectView component to higher level App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Navigation from './navigation.js';
-import Footer from './footer.js';
+import Navigation from './Navigation.js';
+import Footer from './Footer.js';
+import ProjectView from './ProjectView.js';
 
 export default class App extends React.Component {
     render() {
         return (
             <div className="exterior">
                 <Navigation />
+                <ProjectView />
                 <Footer />
             </div>
         )
