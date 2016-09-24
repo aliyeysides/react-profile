e314f32f3bef29f5201abf9a25915cb6e6788f2b
ali yeysides <ali.yeysides@gmail.com>
2016-09-24 12:45:30 -0400

    Create SiestaSuite project component

---
+++ src/components/portfolio/SiestaSuite.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Project from '../Project.js';
+
+export default class SiestaSuite extends Project {
+    render(){
+        return <Project src="../../../public/siesta-harness-example.png" />
+    }
+}
